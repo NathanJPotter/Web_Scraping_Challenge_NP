1894b04444aef8e4e7b2cd25518996833f8a251e
NathanJPotter <82375922+NathanJPotter@users.noreply.github.com>
2021-08-01 16:31:19 +1000

    First try at full scrape.py script

--- Mission_to_Mars/scrape_mars.py
+++ Mission_to_Mars/scrape_mars.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[437]:
-
-
 import pandas as pd
 from splinter import Browser
 from bs4 import BeautifulSoup as bs
 import time
 from webdriver_manager.chrome import ChromeDriverManager
-
-
-# In[438]:
 
 
 # Set up Splinter
@@ -44,126 +38,51 @@ def mars_news(browser):
 # # Scape Featured Mars Space Image
 
 def featured_image(browser):
+    # Visit https://spaceimages-mars.com/
+    image_url = "https://spaceimages-mars.com/"
+    browser.visit(image_url)
 
+    browser.is_elemnent_present_by_css('div.floating_text_area', wait_time=1)
 
-# Visit https://spaceimages-mars.com/
-image_url = "https://spaceimages-mars.com/"
-browser.visit(image_url)
+    # Scape image_page into Soup
+    image_html = browser.html
+    image_soup = bs(image_html, "html.parser")
 
-browser.is_elemnent_present_by_css('div.floating_text_area', wait_time=1)
+    try:
+        button_elem = image_soup.select_one('div.floating_text_area')
+        full_image_button = button_elem.select_one('a').get("href")
+        featured_image_url = image_url + full_image_button
 
-# Scape image_page into Soup
-image_html = browser.html
-image_soup = bs(image_html, "html.parser")
-
-
-# In[446]:
-
-
-# Find FULL IMAGE button
-button_elem = image_soup.select_one('div.floating_text_area')
-button_elem
-
-
-# In[447]:
-
-
-# Get FULL IMAGE link
-full_image_button = button_elem.select_one('a').get("href")
-full_image_button
-
-
-# In[448]:
-
-
-# Add FULL IMAGE link to base url to get featured image
-featured_image_url = image_url + full_image_button
-featured_image_url
+    except AttributeError:
+        return None, None
+    return featured_image_url
 
 
 # # Scrape Mars Facts
 
-# In[449]:
-
-
-# Get Mars comparison table with pandas 'pd.read_html'
-
-mars_comp_df = pd.read_html('https://galaxyfacts-mars.com/')[0]
-mars_comp_df.head()
-
-
-# In[450]:
-
-
-# Rename table columns
-mars_comp_df.columns = ['Mars - Earth Comparison', 'Mars', 'Earth']
-mars_comp_df.set_index('Mars - Earth Comparison', inplace=True)
-mars_comp_df
-
-
-# In[451]:
-
-
-mars_facts_df = mars_comp_df[1:].reset_index()
-mars_facts_df
-
+def mars_facts():
+    mars_comp_df = pd.read_html('https://galaxyfacts-mars.com/')[0]
+    mars_comp_df.columns = ['Mars - Earth Comparison', 'Mars', 'Earth']
+    mars_comp_df.set_index('Mars - Earth Comparison', inplace=True)
+    mars_facts_df = mars_comp_df[1:].reset_index()
+    mars_facts_df
 
 # # Scrape Images and Titles of Mars Hemispheres
 
 # In[452]:
+def hemisphere(browser):
+    hem_url = "https://marshemispheres.com/"
+    browser.visit(hem_url)
 
+    hem_html = browser.html
+    hem_soup = bs(hem_html, "html.parser")
 
-hem_url = "https://marshemispheres.com/"
-browser.visit(hem_url)
+    hemisphere_image_urls = []
 
-time.sleep(1)
-
-
-# In[453]:
-
-
-hem_html = browser.html
-hem_soup = bs(hem_html, "html.parser")
-
-
-# In[454]:
-
-
-# hem_elem = hem_soup.select_one("div.item")
-# hem_elem
-
-
-# In[455]:
-
-
-# hem_links = hem_elem.select_one('a').get("href")
-# hem_links
-
-
-# In[456]:
-
-
-# links_url = hem_url + hem_links
-# links_url
-
-
-# In[457]:
-
-
-# links = browser.find_by_css('h3').click()
-# links
-
-
-# In[464]:
-
-
-hemisphere_image_urls = []
-
-links = browser.find_by_tag('h3')
+    links = browser.find_by_tag('h3')
 
 for item in links:
     hemisphere = {}
-    
     # Loop through each element and click on hemisphere links
     browser.find_by_tag('h3').click()    
         
@@ -180,12 +99,50 @@ for item in links:
     
     # Return to previous webpage
     browser.back()
+return hemisphere_image_urls
+
+# hem_url = "https://marshemispheres.com/"
+# browser.visit(hem_url)
+
+# time.sleep(1)
 
 
-# In[465]:
+# # In[453]:
 
 
-hemisphere_image_urls
+# hem_html = browser.html
+# hem_soup = bs(hem_html, "html.parser")
+
+
+# hemisphere_image_urls = []
+
+# links = browser.find_by_tag('h3')
+
+# for item in links:
+#     hemisphere = {}
+    
+#     # Loop through each element and click on hemisphere links
+#     browser.find_by_tag('h3').click()    
+        
+#     # Get hemisphere title
+#     hemisphere["title"] = browser.find_by_css("h2.title").text
+    
+#     # Get url for full resolution image of hemisphere
+#     sample_link = browser.find_by_text("Sample").first
+#     hemisphere["img_url"] = sample_link["href"]
+
+    
+#     # Append hemisphere object to list
+#     hemisphere_image_urls.append(hemisphere)
+    
+#     # Return to previous webpage
+#     browser.back()
+
+
+# # In[465]:
+
+
+# hemisphere_image_urls
 
 
 # In[ ]:
@@ -195,7 +152,24 @@ hemisphere_image_urls
 
 
 # In[ ]:
+def scrape_all():
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
+    latest_news_title, news_para = mars_news(browser)
+    featured_image_url = featured_image(browser)
+    mars_facts_df = mars_facts()
+    hemisphere_image_urls = hemisphere(browser)
+    timestamp = dt.datetime.now()
 
+    data = {
+        "latest_news_title": latest_news_title,
+        "news_para": news_para,
+        "featured_image_url": featured_image(browser),
+        "mars_facts": mars_facts(),
+        "hemispheres": hemisphere(browser),
+        "last_modified": dt.datetime.now()
+    }
 
-browser.quit()
+    browser.quit()
+    return data
 
